57b52c72ab6aee052e37c14e46b3d6734d97c9e6
Ke Chen <101995749+choriius@users.noreply.github.com>
2022-07-21 05:20:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,76 @@
+//color score variables
+var color = ["tangerine",  //1
+             "peach",      //2
+             "lemon",      //3
+             "bubble",     //4
+             "white",      //5
+             "beige",      //6
+             "ivory",      //7
+             "gold",       //8
+             "azure",      //9
+             "rainbow",    //10
+             "sage",       //11
+             "lavender",   //12
+             "indicolite"];//13
+
+var score = [];
+var winColor = [];
+
+//resets page position
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+//Detects Clicks
+document.getElementById("q1a2").className += " selected";
+
+
+
+
+
+
+//Calculation from Questions
+
+
+
+
+
+function setScoreAll(list, value) {
+  score = [];
+  winColor = [];
+  
+  for (let i = 0; i < color.length; i++) {
+    score.push(0);
+  }
+}
+
+function add (index, amount) {
+  var before = score[index];
+  score[index] = before + amount;  
+}
+
+function winIndex () {
+  var index = [];
+  var max = Math.max.apply(null, score);
+  console.log("Max score = " + max);
+  
+  for (let i1 = 0; i1 < score.length; i1++) {
+    if (score[i1] == max) {
+      index.push(i1);
+    }
+  }
+  return index;
+
+}
+
+/*Testing
+setScoreAll(score, 0);
+console.log(score.join(" ,") + "   length:" + score.length);
+
+add(3, 3);
+add(4, 3);
+add(6, 3);
+
+console.log(score.join(" ,") + "   length:" + score.length);
+
+console.log(winIndex().join(" ,")); */
